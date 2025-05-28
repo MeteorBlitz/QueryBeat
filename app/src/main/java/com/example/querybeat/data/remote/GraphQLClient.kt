1bf6dc8a7e5e9e3974d5957831338798aac141d1
deepak <dlipkumar77@gmail.com>
2025-05-28 13:12:01 +0530

    chore: add Constants file for Apollo Client base URL

--- app/src/main/java/com/example/querybeat/data/remote/GraphQLClient.kt
+++
@@ -1,9 +0,0 @@
-package com.example.querybeat.data.remote
-
-import com.apollographql.apollo.ApolloClient
-
-object GraphQLClient {
-    val apolloClient: ApolloClient = ApolloClient.Builder()
-        .serverUrl("https://countries.trevorblades.com/graphql")
-        .build()
-}
---
+++ app/src/main/java/com/example/querybeat/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.querybeat.util
+
+object Constants {
+    const val GRAPHQL_BASE_URL = "https://countries.trevorblades.com/graphql"
+}
